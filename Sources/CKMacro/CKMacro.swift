56e49da3184ab3e79c0eff8edd75212b4fc626cb
Visckmart <visckmart@hotmail.com>
2024-08-31 23:43:37 -0300

    Encapsulate the macro definition file.

--- Sources/CKMacro/CKMacro.swift
+++ Sources/CKMacro/CKMacro.swift
@@ -1,84 +1,16 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import Foundation
 
-@attached(member, names: named(x), named(convertToCKRecord), named(init(fromCKRecord:fetchingRelationshipsFrom:)), named(CKRecordDecodingError), named(__recordID), named(__recordType), named(__recordZoneID), named(__recordName), named(CKRecordEncodingError), named(__storedCKRecord))
+@attached(member, names: named(x),
+          named(init(fromCKRecord:fetchingRelationshipsFrom:)), named(convertToCKRecord), named(__recordType),
+          named(CKRecordEncodingError), named(CKRecordDecodingError),
+          named(__recordID), named(__recordZoneID), named(__recordName), named(__storedCKRecord))
 @attached(extension, conformances: SynthesizedCKRecordConvertible)
 public macro ConvertibleToCKRecord(recordType: String? = nil) = #externalMacro(module: "CKMacroMacros", type: "ConvertibleToCKRecordMacro")
 
-import CloudKit
-
-public protocol SynthesizedCKRecordConvertible: CKIdentifiable {
-    func convertToCKRecord(usingBaseCKRecord: CKRecord?) throws -> (CKRecord, [CKRecord])
-    init(fromCKRecord ckRecord: CKRecord, fetchingRelationshipsFrom: CKDatabase?) async throws
-    mutating func saveToCKDatabase(_ database: CKDatabase, usingBaseCKRecord: CKRecord?) async throws
-    static var __recordType: String { get }
-}
-
-public extension SynthesizedCKRecordConvertible {
-    func saveToCKDatabase(_ database: CKDatabase, usingBaseCKRecord baseCKRecord: CKRecord? = nil) async throws {
-        let (ckRecord, relationshipRecords) = try self.convertToCKRecord(usingBaseCKRecord: baseCKRecord)
-        if #available(macOS 12.0, *) {
-            let (saveResults, _) = try await database.modifyRecords(
-                saving: [ckRecord] + relationshipRecords,
-                deleting: [],
-                savePolicy: .allKeys,
-                atomically: true
-            )
-            print(saveResults)
-        } else {
-            try await database.save(ckRecord)
-            for relationshipRecord in relationshipRecords {
-                try await database.save(relationshipRecord)
-            }
-        }
-    }
-    
-    static func fecth(withRecordName recordName: String, fromCKDatabase database: CKDatabase) async throws -> Self {
-        let fetchedRecord = try await database.record(for: CKRecord.ID(recordName: recordName))
-        return try await Self(fromCKRecord: fetchedRecord, fetchingRelationshipsFrom: database)
-    }
-    
-    static func fecthAll(fromCKDatabase database: CKDatabase) async throws -> [Self] {
-        let query = CKQuery(recordType: Self.__recordType, predicate: NSPredicate(value: true))
-        var (response, cursor) = try await database.records(matching: query)
-        
-        
-        var decodedResults = [Self]()
-        repeat {
-            let fetchedRecords = response.compactMap({ try? $0.1.get() })
-            for record in fetchedRecords {
-                let newInstance = try await Self(fromCKRecord: record, fetchingRelationshipsFrom: database)
-                decodedResults.append(newInstance)
-            }
-            if let currentCursor = cursor {
-                (response, cursor) = try await database.records(continuingMatchFrom: currentCursor)
-            }
-        } while cursor != nil
-        return decodedResults
-    }
-}
-extension SynthesizedCKRecordConvertible where Self: Codable {
-    public static func fetch(fromDatabase database: CKDatabase, key: CodingKey, value: NSObject) async throws -> (matchResults: [(CKRecord.ID, Result<CKRecord, any Error>)], queryCursor: CKQueryOperation.Cursor?) {
-        let predicate = NSPredicate(format: "%K = %@", NSString(string: key.stringValue), value)
-        let query = CKQuery(recordType: Self.__recordType, predicate: predicate)
-        return try await database.records(matching: query)
-    }
-}
-public protocol CKRecordSynthetizationDelegate: SynthesizedCKRecordConvertible {
-    func willFinishEncoding(ckRecord: CKRecord) throws
-    func willFinishDecoding(ckRecord: CKRecord) throws
-}
-
-public extension CKRecordSynthetizationDelegate {
-    public func willFinishEncoding(ckRecord: CKRecord) throws { }
-    public func willFinishDecoding(ckRecord: CKRecord) throws { }
-}
-
-public protocol CKIdentifiable {
-    var __recordID: CKRecord.ID { get }
-    var __recordName: String { get }
-//    var __recordZoneID: CKRecordZone.ID? { get set }
-}
+@attached(peer)
+public macro CKRecordName() = #externalMacro(module: "CKMacroMacros", type: "CKRecordNameMacro")
 
 public enum ReferenceType {
     case referencesProperty
@@ -90,18 +22,12 @@ public enum ReferenceType {
 @attached(peer)
 public macro CKReference(_ referenceType: ReferenceType) = #externalMacro(module: "CKMacroMacros", type: "RelationshipMarkerMacro")
 
-@attached(peer)
-public macro CKRecordName() = #externalMacro(module: "CKMacroMacros", type: "CKRecordNameMacro")
-//@attached(peer)
-//public macro CKEncode(with: (CKRecord) -> CKRecordValue) = #externalMacro(module: "CKMacroMacros", type: "RelationshipMarkerMacro2")
-//
 public enum PropertyType {
     case rawValue
 }
 @attached(peer)
 public macro CKPropertyType(_ propertyType: PropertyType) = #externalMacro(module: "CKMacroMacros", type: "CKPropertyTypeMacro")
-//@attached(peer)
-//public macro CKDecode(with: () -> CKRecordValue) = #externalMacro(module: "CKMacroMacros", type: "RelationshipMarkerMacro2")
+
 
 @dynamicMemberLookup
 public struct CodableWrapper<A: NSCoding & NSObject>: Codable {
---
+++ Sources/CKMacro/ProtocolDefinitions.swift
@@ -0,0 +1,77 @@
+//
+//  SynthesizedCKRecordConvertible.swift
+//  CKMacro
+//
+//  Created by Victor Martins on 31/08/24.
+//
+
+import Foundation
+import CloudKit
+
+public protocol CKIdentifiable {
+    var __recordID: CKRecord.ID { get }
+    var __recordName: String { get }
+}
+
+public protocol SynthesizedCKRecordConvertible: CKIdentifiable {
+    init(fromCKRecord ckRecord: CKRecord, fetchingRelationshipsFrom: CKDatabase?) async throws
+    func convertToCKRecord(usingBaseCKRecord: CKRecord?) throws -> (CKRecord, [CKRecord])
+    mutating func saveToCKDatabase(_ database: CKDatabase, usingBaseCKRecord: CKRecord?) async throws
+    static var __recordType: String { get }
+}
+
+public extension SynthesizedCKRecordConvertible {
+    func saveToCKDatabase(_ database: CKDatabase, usingBaseCKRecord baseCKRecord: CKRecord? = nil) async throws {
+        let (ckRecord, relationshipRecords) = try self.convertToCKRecord(usingBaseCKRecord: baseCKRecord)
+        if #available(macOS 12.0, *) {
+            let (saveResults, _) = try await database.modifyRecords(
+                saving: [ckRecord] + relationshipRecords,
+                deleting: [],
+                savePolicy: .allKeys,
+                atomically: true
+            )
+        } else {
+            try await database.save(ckRecord)
+            for relationshipRecord in relationshipRecords {
+                try await database.save(relationshipRecord)
+            }
+        }
+    }
+}
+    
+extension SynthesizedCKRecordConvertible {
+    static func fetch(withRecordName recordName: String, fromCKDatabase database: CKDatabase) async throws -> Self {
+        let fetchedRecord = try await database.record(for: CKRecord.ID(recordName: recordName))
+        return try await Self(fromCKRecord: fetchedRecord, fetchingRelationshipsFrom: database)
+    }
+    
+    static func fetchAll(fromCKDatabase database: CKDatabase) async throws -> [Self] {
+        let query = CKQuery(recordType: Self.__recordType, predicate: NSPredicate(value: true))
+        var (response, cursor) = try await database.records(matching: query)
+        
+        var decodedResults = [Self]()
+        repeat {
+            let fetchedRecords = response.compactMap({ try? $0.1.get() })
+            for record in fetchedRecords {
+                let newInstance = try await Self(fromCKRecord: record, fetchingRelationshipsFrom: database)
+                decodedResults.append(newInstance)
+            }
+            if let currentCursor = cursor {
+                (response, cursor) = try await database.records(continuingMatchFrom: currentCursor)
+            }
+        } while cursor != nil
+        return decodedResults
+    }
+}
+
+public protocol CKRecordSynthetizationDelegate: SynthesizedCKRecordConvertible {
+    func willFinishEncoding(ckRecord: CKRecord) throws
+    func willFinishDecoding(ckRecord: CKRecord) throws
+}
+
+public extension CKRecordSynthetizationDelegate {
+    public func willFinishEncoding(ckRecord: CKRecord) throws { }
+    public func willFinishDecoding(ckRecord: CKRecord) throws { }
+}
+
+
