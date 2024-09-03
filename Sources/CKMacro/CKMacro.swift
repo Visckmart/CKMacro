018bf0e5dac3b3053b81934e073e8d621cccff05
Visckmart <visckmart@hotmail.com>
2024-09-03 01:00:01 -0300

    Improve documentation.

--- Sources/CKMacro/CKMacro.swift
+++ Sources/CKMacro/CKMacro.swift
@@ -1,7 +1,9 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 import Foundation
+
 @_exported import CloudKit
+
 @attached(member, names: named(x),
           named(init(fromCKRecord:fetchingRelationshipsFrom:)), named(convertToCKRecord), named(__recordType),
           named(CKRecordEncodingError), named(CKRecordDecodingError),
@@ -9,54 +11,48 @@ import Foundation
 @attached(extension, conformances: SynthesizedCKRecordConvertible)
 public macro ConvertibleToCKRecord(recordType: String? = nil, debug: Bool = false) = #externalMacro(module: "CKMacroMacros", type: "ConvertibleToCKRecordMacro")
 
+
+/// Marks the property that will be used by the `@ConvertibleToCKRecord` macro as the `recordName` field
+/// on the generated `CKRecord`s.
+///
+/// - Remark: This macro can only be attached to properties of `String` type.
 @attached(peer)
 public macro CKRecordName() = #externalMacro(module: "CKMacroMacros", type: "CKRecordNameMacro")
 
+
 public enum ReferenceType {
+    /// Generates a field on this instance's `CKRecord` with a reference to the `recordName`
+    /// of the property.
     case referencesProperty
-    case isReferencedByProperty(named: String? = nil, weakReference: Bool = true)
-//    case isStronglyReferencedByProperty
-    public static let isReferencedByProperty = isReferencedByProperty()
-//    case data
+    
+    /// Generates a new field on the CKRecord of the property pointing to this instance's `recordName`.
+    /// The name of the field is specified by the argument `named`.
+    case isReferencedByProperty(named: String?)
+    
+    /// Generates a new field on the CKRecord of the property pointing to this instance's `recordName`.
+    public static let isReferencedByProperty = isReferencedByProperty(named: nil)
 }
+/// Marks the property that should act as a reference.
 @attached(peer)
 public macro CKReference(_ referenceType: ReferenceType) = #externalMacro(module: "CKMacroMacros", type: "RelationshipMarkerMacro")
 
+
 public enum PropertyType {
+    /// Intended to use with enums. Encodes and decodes the value of the enum as its raw value.
+    /// The enum type must have a raw value defined.
     case rawValue
+    
+    /// Intended to use with Codable types.
+    /// Encodes and decodes the value as Data.
     case codable
+    
+    /// Intended to use with types conforming to the NSCoding protocol.
+    /// Encodes and decodes the value as Data.
     case nsCoding
+    
+    /// Is ignored by the macro. Must be an optional or have a default value.
     case ignored
 }
+/// Marks the property with a special way of encoding and decoding.
 @attached(peer)
 public macro CKPropertyType(_ propertyType: PropertyType) = #externalMacro(module: "CKMacroMacros", type: "CKPropertyTypeMacro")
-
-
-@dynamicMemberLookup
-public struct CodableWrapper<A: NSCoding & NSObject>: Codable {
-    
-    var value: A
-    
-    public init(value: A) {
-        self.value = value
-    }
-    public subscript<T>(dynamicMember keyPath: KeyPath<A, T>) -> T {
-        return self.value[keyPath: keyPath]
-    }
-    
-    public subscript<T>(dynamicMember keyPath: WritableKeyPath<A, T>) -> T {
-        get { self.value[keyPath: keyPath] }
-        set { self.value[keyPath: keyPath] = newValue }
-    }
-    public func encode(to encoder: any Encoder) throws {
-        var container = encoder.singleValueContainer()
-        let data = try NSKeyedArchiver.archivedData(withRootObject: value, requiringSecureCoding: false)
-        try container.encode(data)
-    }
-    
-    public init(from decoder: any Decoder) throws {
-        let container = try decoder.singleValueContainer()
-        let data = try container.decode(Data.self)
-        self.value = try NSKeyedUnarchiver.unarchivedObject(ofClass: A.self, from: data)!
-    }
-}
